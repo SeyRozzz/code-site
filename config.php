aaafa41db896d070d9dd3a4e270ec59c93cf017e
Enzo <enzofrichet2@gmail.com>
2026-03-24 15:06:30 +0100

    oui

--- config.php
+++ config.php
@@ -9,8 +9,7 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // ✅ Active les erreurs SQL
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // ✅ Retourne des tableaux associatifs
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // ✅ Meilleure sécurité
+
 ];
 
 try {
--- index.php
+++ index.php
@@ -5,22 +5,18 @@ require_once 'Router.php';
 
 $router = new Router();
 
-// Routes principales
 $router->addRoute('accueil', 'accueil.php');
 $router->addRoute('carte', 'carte.php');
-
-// Routes Authentification
 $router->addRoute('login', 'login.php');
 $router->addRoute('logout', 'logout.php');
-
-// Routes Administration
 $router->addRoute('admin', 'admin.php');
 $router->addRoute('adminAdduser', 'adminAdduser.php');
 $router->addRoute('changer_role', 'changer_role.php');
 $router->addRoute('supprimer_user', 'supprimer_user.php');
-// Route suppression d'arbres
 $router->addRoute('supprimer', 'supprimer.php');
 $router->addRoute('modifier', 'modifierarbre.php');
+// Nouveau
+$router->addRoute('projets', 'projets.php');
+$router->addRoute('supprimer_projet', 'supprimer_projet.php');
 
-// Exécution
 $router->execute($_GET['page'] ?? 'accueil', $pdo);
